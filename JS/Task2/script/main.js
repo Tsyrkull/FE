2a2bb2333d0ebce7b0374c172262e50132194a7d
Tsyrkull <artem13@gmail.com>
2019-12-03 17:00:10 +0200

    init: start JS Task-2

---
+++ JS/Task2/script/main.js
@@ -0,0 +1,8 @@
+let array=[]
+    ,newArray=[];
+for (let i=0;i<16;i++){
+    console.log(`i в десятичной: ${i}, i в двоичной: ${i.toString(2)}`);
+    array.push(i.toString(2));
+}
+console.log(array);
+let mask = '1u 2d 0u 3d';
