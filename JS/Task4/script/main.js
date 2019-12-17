41a9b81790ef5d867e404407f4e815549da509b3
Tsyrkull <artem13@gmail.com>
2019-12-17 18:20:33 +0200

    feat: implement JS Task-4 draft

---
+++ JS/Task4/script/main.js
@@ -0,0 +1,37 @@
+let startQuest = confirm(`Do you want to play a game?`),
+    minNum = 0,
+    maxNum = 100,
+    attemptNum = 1,
+    tryNums = [],
+    userNum = +prompt(`Enter a number from ${minNum} to ${maxNum} and computer will try to guess it`);
+
+while (startQuest) {
+    let compNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+        tryNums.push(compNum);
+    console.log(compNum);
+    if (userNum) {
+        if (userNum === compNum) {
+            let newGame = confirm(`Computer choose ${compNum}.\nComputer guessed your number from ${attemptNum} attempts!\nDo you want to play again?`);
+            if (newGame) {
+                attemptNum = 1;
+                tryNums = [];
+                userNum = +prompt('Enter a number from ' + minNum + ' to ' + maxNum);
+                compNum = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+
+            } else {
+                alert(`Thank you for your game. Have a nice day!`);
+                break
+            }
+        } else {
+            alert(`Computer choose ${compNum}.\nComputer missed, and now he will try again`);
+            attemptNum++;
+        }
+
+    } else {
+        alert(`You failed the input`);
+        break
+    }
+}
+if (!startQuest) {
+    alert(`Let's play next time`)
+}
